35cb730542b75985657aee2513c4a61a0b48a063
Elham Mirzavand <elham.mirzavand@gmail.com>
2024-02-20 22:06:41 +0100

    add project

---
+++ Kattis/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static string IsRepeated(string text)
+        {
+            var words = text.Split(" ");
+            int numberOfRepeatedWords = 0;
+            bool isRightFormat = true;
+            if (text.Count() > 80)
+            {
+               return "invalid input";
+            }
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                if (words[i].Any(char.IsUpper))
+                {
+                    isRightFormat = isRightFormat && true;
+                }
+                else
+                {
+                    isRightFormat = isRightFormat && false;
+                    return "invalid input";
+                }
+            }
+            if (isRightFormat)
+            {
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    for(int j=i+1; j < words.Length - 1; j++)
+                    {
+                        if (words[i] == words[j])
+                        {
+                            numberOfRepeatedWords++;
+                        }
+                    }
+                    
+                }
+            }
+            if (numberOfRepeatedWords == 0)
+            {
+                return "Yes";
+
+            }
+            else
+            {
+                return "No";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number or 'ok' to end or enter your string: ");
+            string input = "";
+            string result = "";
+            while (input?.ToLower() != "ok")
+            {
+                input = Console.ReadLine();
+                result = IsRepeated(input);
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
+
+
